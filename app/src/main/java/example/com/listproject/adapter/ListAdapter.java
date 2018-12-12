05cde734d7ed7f9421fd58a2976cd047b7fb3903
Javier Aranda Caro <javierarandacaro@gmail.com>
2018-12-12 19:12:01 +0100

    Adapter and Interactor finished.

--- app/src/main/java/example/com/listproject/adapter/ListAdapter.java
+++ app/src/main/java/example/com/listproject/adapter/ListAdapter.java
@@ -2,14 +2,38 @@ package example.com.listproject.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import example.com.listproject.R;
 import example.com.listproject.data.pojo.Mensaje;
 import example.com.listproject.data.repository.MessageRepository;
 
 public class ListAdapter extends ArrayAdapter<Mensaje> {
 
     public ListAdapter(@NonNull Context context) {
-        super(context, layu, MessageRepository.getRepository().getMensajes());
+        super(context, R.layout.list_item, MessageRepository.getRepository().getMensajes());
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        TextView tvAutor;
+        TextView tvMensaje;
+        View view;
+
+        LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(R.layout.list_item, null);
+        tvAutor = view.findViewById(R.id.tvAutor);
+        tvMensaje = view.findViewById(R.id.tvMensaje);
+
+        tvAutor.setText(getItem(position).getAutor());
+        tvMensaje.setText(getItem(position).getMensaje());
+
+        return view;
     }
 }
--- app/src/main/java/example/com/listproject/ui/ListInteractor.java
+++ app/src/main/java/example/com/listproject/ui/ListInteractor.java
@@ -1,4 +1,38 @@
 package example.com.listproject.ui;
 
+import android.os.AsyncTask;
+
+import java.util.List;
+
+import example.com.listproject.data.pojo.Mensaje;
+import example.com.listproject.data.repository.MessageRepository;
+
 public class ListInteractor {
+
+    ListInteractor.onLoadMensajeItems onLoadMensajeItems;
+
+    public interface onLoadMensajeItems{
+        void onSuccess(List<Mensaje> mensajes);
+        void onError();
+    }
+
+    public ListInteractor(ListPresenter presenter){
+        this.onLoadMensajeItems = presenter;
+    }
+
+
+    public void loadMensajes(){
+        new AsyncTask<Void, Void, List<Mensaje>>() {
+            @Override
+            protected List<Mensaje> doInBackground(Void... voids) {
+                return MessageRepository.getRepository().getMensajes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Mensaje> mensajes) {
+                onLoadMensajeItems.onSuccess(mensajes);
+            }
+        }.execute();
+    }
+
 }
